f6ae376de168ec41014c4f764aa92d630f2e2ab3
Rafiul <104949652+Rafiul09@users.noreply.github.com>
2024-06-03 14:58:40 +0600

    Update Addition.asm

--- Mid/Addition.asm
+++ Mid/Addition.asm
@@ -18,59 +18,59 @@ main proc
     mov ax,@data
     mov ds,ax
     
-    mov ah,9
+    mov ah,9     ;disp db enter number 1: 
     lea dx,in1
     int 21h
     
-    mov ah,1
+    mov ah,1     ;inp 1
     int 21h
     mov bh,al 
     
-    mov ah,2
+    mov ah,2     ;nl
     mov dl,10
     int 21h
     mov dl,13
     int 21h   
     
-     mov ah,9
+     mov ah,9    ;disp db  enter number 2: 
     lea dx,in2
     int 21h
     
-    mov ah,1
+    mov ah,1     ;inp 2
     int 21h
     mov bl,al
           
-    mov ah,9
+    mov ah,9     ;nl
     lea dx,nl
     int 21h
       
     
-     mov ah,9
+     mov ah,9    ;disp db Addition of 
     lea dx,pr1
     int 21h
     
     
     
-    mov ah,2
+    mov ah,2     ;disp  in1
     mov dl,bh
     int 21h 
        
     
-     mov ah,9
+     mov ah,9    ;disp db and
     lea dx,pr2
     int 21h
    
-    mov ah,2
+    mov ah,2     ;disp   inp2
     mov dl,bl
     int 21h 
     
-     mov ah,9
+     mov ah,9    ;disp db is 
     lea dx,pr3
     int 21h   
     
  
     
-    add bh,bl
+    add bh,bl      ;add
     sub bh,30h
     
     mov ah, 2
