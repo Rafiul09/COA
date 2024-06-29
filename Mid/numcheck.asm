e4a60a9cae2b2895b55f738d2994d55dd8c4b519
Rafiul <104949652+Rafiul09@users.noreply.github.com>
2024-06-29 14:06:06 +0600

    Update numcheck.asm

--- Mid/numcheck.asm
+++ Mid/numcheck.asm
@@ -8,13 +8,14 @@ e2 db 'Enter 2nd number: $'
 p db 10,13,'1st number is greater$'
 q db 10,13,'2nd number is greater$'
 eq db 10,13,'Both numbers are equal$'
-i db 10,13,'Invalid number$'
+i db 10,13,'Invalid number$' 
+nl db 10,13,'$'
 .code
 main proc
     mov ax, @data
     mov ds, ax
 
-    ; Get the first number
+    ;first number
     mov ah, 9
     lea dx, e
     int 21h
@@ -23,13 +24,17 @@ main proc
     int 21h   
     mov a, al
 
-    ; Check if input is a valid digit
+    ; Check digit
     cmp a, 30h
     jl invalid_input
     cmp a, 39h
     jg invalid_input
-
-    ; Get the second number
+                         
+    mov ah,9     ;nl
+    lea dx,nl
+    int 21h
+                         
+    ;second number
     mov ah, 9
     lea dx, e2
     int 21h
@@ -38,7 +43,7 @@ main proc
     int 21h  
     mov b, al
 
-    ; Check if input is a valid digit
+    ; Check digit
     cmp b, 30h
     jl invalid_input
     cmp b, 39h
@@ -46,8 +51,7 @@ main proc
 
     ; Compare the two numbers
     mov al, a 
-    mov bl, b
-    cmp al, bl
+    cmp al, b
     je numbers_equal
     ja first_greater
     jb second_greater
@@ -56,26 +60,26 @@ first_greater:
     mov ah, 9
     lea dx, p
     int 21h
-    jmp exit_program
+    jmp exit
 
 second_greater:
     mov ah, 9
     lea dx, q
     int 21h
-    jmp exit_program
+    jmp exit
 
 numbers_equal:
     mov ah, 9
     lea dx, eq
     int 21h
-    jmp exit_program
+    jmp exit
 
 invalid_input:
     mov ah, 9
     lea dx, i
     int 21h
 
-exit_program:
+exit:
     mov ah, 4Ch
     int 21h
 
